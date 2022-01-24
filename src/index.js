a84c9fada4166c3acb2779a18115e61f2f470b75
Cezar <cezarguimaraes@gmail.com>
2022-01-24 17:12:56 -0300

    Rota post/users adicionada

--- src/index.js
+++ src/index.js
@@ -1,41 +1,57 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const { id } = uuidv4();
+
+  newUser = {
+    id,
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(newUser);
+
+  return response.json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userTodos = (users.find((user) => user.username === username)).todos;
+
+  return response.json(userTodos);
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+// app.post('/todos', checksExistsUserAccount, (request, response) => {
+//   // Complete aqui
+// });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+// app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+//   // Complete aqui
+// });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+// app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+//   // Complete aqui
+// });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+// app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+//   // Complete aqui
+// });
 
 module.exports = app;
